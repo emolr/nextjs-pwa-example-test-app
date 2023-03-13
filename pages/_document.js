be1dd6e9a1ba2e4515051144c12e10a975e3707a
emolr <emolr@users.noreply.github.com>
2023-03-13 15:28:37 +0100

    3 files modified, 2 added

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,9 @@ export default function Document() {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="apple-touch-fullscreen" content="yes" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/logo-96x96.png" />
+        <meta name="apple-mobile-web-app-status-bar" content="#90cdf4" />
       </head>
       <body>
         <Main />
