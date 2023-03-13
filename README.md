0d9c5c3de71ed752ef0398a4b5ae02b4bc87d977
emolr <emolr@users.noreply.github.com>
2023-03-13 15:13:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-pwa-example-test-app
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-s4ahpo)
---
+++ pages/subpage.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function SubPage() {
+  return (
+    <div>
+      sub page. - <Link href="./">Home</Link>
+    </div>
+  );
+}
