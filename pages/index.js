8e346c2d3b93be960d874f0451fa6becb8d17763
emolr <emolr@users.noreply.github.com>
2023-03-14 10:08:34 +0100

    1 file modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import { getPWADisplayMode } from '../libs/getPWADisplayMode';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [isPWA, setIsPWA] = useState(false);
+  useEffect(() => {
+    const browserMode = getPWADisplayMode();
+
+    if (browserMode === 'standalone') {
+      setIsPWA(true);
+    }
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
       </Head>
+
+      {isPWA && <h1>This is running as an installed PWA!</h1>}
 
       <Link href="./subpage">Subpage</Link>
       <br />
